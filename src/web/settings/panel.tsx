1df5b2577857da7c6012862bf2d9d66d380405ab
Thien Do <5953369+thien-do@users.noreply.github.com>
2022-08-11 01:29:17 +0700

    fix: Improve Setting panel styling

--- src/web/settings/panel.tsx
+++ src/web/settings/panel.tsx
@@ -13,19 +13,17 @@ export const SettingsPanel = (props: Props): JSX.Element => {
   return (
     <div className={s.wrapper}>
       <div className={s.container}>
-        <div className={s.group}>
-          <SettingsTheme {...props} />
-        </div>
-        <div className={s.group}>
-          <SettingsPreview {...props} />
-        </div>
-        <div className={s.group}>
-          <SettingsVim {...props} />
-          <hr className={s.line} />
-          <SettingsWrapColumn {...props} />
-          <hr className={s.line} />
-          <SettingsFontSize {...props} />
-        </div>
+        {[
+          <SettingsTheme {...props} />,
+          <SettingsPreview {...props} />,
+          <SettingsVim {...props} />,
+          <SettingsWrapColumn {...props} />,
+          <SettingsFontSize {...props} />,
+        ].map((element, index) => (
+          <div className={s.item} key={index}>
+            {element}
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/web/toolbar/settings/settings.tsx
+++ src/web/toolbar/settings/settings.tsx
@@ -1,7 +1,6 @@
+import { shift, useFloating } from "@floating-ui/react-dom";
 import { Popover } from "@headlessui/react";
 import { GearIcon } from "@primer/octicons-react";
-import { useState } from "react";
-import { usePopper } from "react-popper";
 import { LayoutState } from "../../layout/type";
 import { SettingsPanel } from "../../settings/panel";
 import { SettingsState } from "../../settings/type";
@@ -10,25 +9,26 @@ import { ToolbarButton } from "./../button/button";
 interface Props extends SettingsState, LayoutState {}
 
 export const ToolbarSettings = (props: Props): JSX.Element => {
-  const [button, setButton] = useState<HTMLElement | null>(null);
-  const [popover, setPopover] = useState<HTMLElement | null>(null);
-  const { styles, attributes } = usePopper(button, popover);
+  const float = useFloating({ middleware: [shift()] });
 
   return (
     <Popover>
       {({ open }) => (
         <>
           <ToolbarButton
-            ref={setButton}
+            ref={float.reference}
             as={Popover.Button}
             Icon={GearIcon}
             label="Settings"
             selected={open}
           />
           <Popover.Panel
-            ref={setPopover}
-            style={styles.popper}
-            {...attributes.popper}
+            ref={float.floating}
+            style={{
+              position: float.strategy,
+              top: float.y ?? 0,
+              left: float.x ?? 0,
+            }}
           >
             <SettingsPanel {...props} />
           </Popover.Panel>
