246af86eb4d64e4e36395c6ce0600e86cd837aae
Louis Phan <louisphn@LP202205MAC083.local>
2022-08-14 16:00:05 +0900

    feat: Tailwind Preview Template

--- src/web/preview/preview.tsx
+++ src/web/preview/preview.tsx
@@ -1,12 +1,25 @@
 import { Editor } from "../editor/type";
 import { usePreviewHtml } from "./html";
-
+import { PREVIEW_TEMPLATE_DETAILS } from "./template/state";
+import { PreviewTemplateState } from "./template/state";
 interface Props {
   editor: Editor;
+  prefs: PreviewTemplateState; // { previewTemplate: "tailwind" | "github" | "serif", setPreviewTemplate: setState }
 }
 
 export const Preview = (props: Props): JSX.Element => {
-  const { editor } = props;
+  const { editor, prefs } = props;
+  // template as a setting
+  // const template = PREVIEW_TEMPLATE_DETAILS[props.prefs.previewTemplate];
+
+  // tailwind template
+  const tailwindTemplate = PREVIEW_TEMPLATE_DETAILS["tailwind"];
+
   const html = usePreviewHtml({ editor });
-  return <div dangerouslySetInnerHTML={{ __html: html }} />;
+  return (
+    <div
+      className={tailwindTemplate.className}
+      dangerouslySetInnerHTML={{ __html: html }}
+    />
+  );
 };
---
+++ src/web/preview/template/state.ts
@@ -0,0 +1,33 @@
+import { SetState } from "../../utils/state/type";
+import * as styles from "./styles/styles";
+
+export const PREVIEW_TEMPLATE_NAMES = ["tailwind", "github", "serif"] as const;
+
+export type PreviewTemplateName = typeof PREVIEW_TEMPLATE_NAMES[number];
+
+export interface PreviewTemplateDetail {
+  name: string;
+  className: string;
+}
+
+export interface PreviewTemplateState {
+  previewTemplate: PreviewTemplateName;
+  setPreviewTemplate: SetState<PreviewTemplateName>;
+}
+export const PREVIEW_TEMPLATE_DETAILS: Record<
+  PreviewTemplateName,
+  PreviewTemplateDetail
+> = {
+  github: {
+    name: `GitHub`,
+    className: styles.github.container,
+  },
+  tailwind: {
+    name: `Tailwind`,
+    className: styles.tailwind.container,
+  },
+  serif: {
+    name: "Serif",
+    className: styles.serif.container,
+  },
+};
