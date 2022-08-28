f43ab5045f10e9b58da01170fef1ee8444a59c96
Thien Do <5953369+thien-do@users.noreply.github.com>
2022-08-28 13:31:24 +0700

    fix

--- src/web/preview/preview.tsx
+++ src/web/preview/preview.tsx
@@ -1,25 +1,20 @@
 import { Editor } from "../editor/type";
+import { Settings } from "../settings/type";
+import { TemplateContainer } from "../template/template";
 import { usePreviewHtml } from "./html";
-import { PREVIEW_TEMPLATE_DETAILS } from "./template/state";
-import { PreviewTemplateState } from "./template/state";
+
 interface Props {
   editor: Editor;
-  prefs: PreviewTemplateState; // { previewTemplate: "tailwind" | "github" | "serif", setPreviewTemplate: setState }
+  settings: Settings;
 }
 
 export const Preview = (props: Props): JSX.Element => {
-  const { editor, prefs } = props;
-  // template as a setting
-  // const template = PREVIEW_TEMPLATE_DETAILS[props.prefs.previewTemplate];
-
-  // tailwind template
-  const tailwindTemplate = PREVIEW_TEMPLATE_DETAILS["tailwind"];
+  const { editor, settings } = props;
 
   const html = usePreviewHtml({ editor });
   return (
-    <div
-      className={tailwindTemplate.className}
-      dangerouslySetInnerHTML={{ __html: html }}
-    />
+    <TemplateContainer template={settings.template}>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+    </TemplateContainer>
   );
 };
--- src/web/preview/template/styles/styles.ts
+++
@@ -1,1 +0,0 @@
-export { default as tailwind } from "./tailwind/tailwind";
