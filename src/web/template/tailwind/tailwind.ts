456175564bc96bd9cc917eb1e62b15fe4d229e1b
Thien Do <5953369+thien-do@users.noreply.github.com>
2022-08-28 14:50:07 +0700

    fix(web): Fix css imports

--- src/web/template/tailwind/tailwind.ts
+++ src/web/template/tailwind/tailwind.ts
@@ -1,5 +1,5 @@
 // "base" must comes before "typography"
-import base from "./base.module.css";
-import typography from "./typography.module.css";
+import * as base from "./base.module.css";
+import * as typography from "./typography.module.css";
 
 export const tailwindTemplate = `${base.container} ${typography.container}`;
