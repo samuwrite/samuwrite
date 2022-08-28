ecca30800608aecc0261de283a9f961957ba2a42
Thien Do <5953369+thien-do@users.noreply.github.com>
2022-08-28 13:41:18 +0700

    fix

--- src/web/preview/preview.tsx
+++ src/web/preview/preview.tsx
@@ -2,6 +2,7 @@ import { Editor } from "../editor/type";
 import { Settings } from "../settings/type";
 import { TemplateContainer } from "../template/template";
 import { usePreviewHtml } from "./html";
+import * as s from "./preview.module.css";
 
 interface Props {
   editor: Editor;
@@ -12,9 +13,12 @@ export const Preview = (props: Props): JSX.Element => {
   const { editor, settings } = props;
 
   const html = usePreviewHtml({ editor });
+
   return (
-    <TemplateContainer template={settings.template}>
-      <div dangerouslySetInnerHTML={{ __html: html }} />
-    </TemplateContainer>
+    <div className={s.container}>
+      <TemplateContainer template={settings.template}>
+        <div dangerouslySetInnerHTML={{ __html: html }} />
+      </TemplateContainer>
+    </div>
   );
 };
