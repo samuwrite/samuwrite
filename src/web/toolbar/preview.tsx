c537b1e6a10e5110f15e1c0c3ce680db138be3bb
Thien Do <5953369+thien-do@users.noreply.github.com>
2022-08-29 22:49:40 +0700

    feat(web): Add hotkey to toggle layout

--- src/web/toolbar/preview.tsx
+++ src/web/toolbar/preview.tsx
@@ -1,4 +1,6 @@
 import { LogIcon } from "@primer/octicons-react";
+import { useCallback, useEffect } from "react";
+import tinykeys from "tinykeys";
 import { Layout, LayoutState } from "../layout/type";
 import { getLayoutFromPreview } from "../settings/preview/preview";
 import { Settings } from "../settings/type";
@@ -8,19 +10,28 @@ interface Props extends LayoutState {
   settings: Settings;
 }
 
-const getLayout = (props: Props): Layout => {
-  const { layout, settings } = props;
-  if (layout !== "editor") return "editor";
-  return getLayoutFromPreview(settings.preview);
-};
-
 export const ToolbarPreview = (props: Props): JSX.Element => {
-  const { layout, setLayout } = props;
+  const { layout, settings, setLayout } = props;
+
+  const toggleLayout = useCallback(() => {
+    const target: Layout =
+      layout === "editor" ? getLayoutFromPreview(settings.preview) : "editor";
+    setLayout(target);
+  }, [layout, settings, setLayout]);
+
+  useEffect(() => {
+    const unsub = tinykeys(window, {
+      "$mod+r": (event) => {
+        event.preventDefault();
+        toggleLayout();
+      },
+    });
+    return () => unsub();
+  }, [toggleLayout]);
+
   return (
     <ToolbarButton
-      onClick={() => {
-        setLayout(getLayout(props));
-      }}
+      onClick={toggleLayout}
       Icon={LogIcon}
       label="Preview"
       selected={layout !== "editor"}
