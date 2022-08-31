ab4ed59824b5f94f51b793eb2dbd54d41bdf3872
Thien Do <5953369+thien-do@users.noreply.github.com>
2022-08-31 22:00:55 +0700

    refactor(mac): Clean mouseDown drag

--- src/mac/SamuwriteNative/Scenes/DraggableWebView.swift
+++ src/mac/SamuwriteNative/Scenes/DraggableWebView.swift
@@ -10,44 +10,11 @@ import AppKit
 
 final class DraggableWebView: WKWebView {
     override func mouseDown(with event: NSEvent) {
-        guard let window = self.window else { return }
-        let startingPoint = event.locationInWindow
-        // Track events until the mouse is up (in which we interpret as a click), or a drag starts (in which we pass off to the Window Server to perform the drag)
-        var shouldCallSuper = false
-        
-        window.trackEvents(
-            matching: [.leftMouseDragged, .leftMouseUp],
-            timeout: NSEvent.foreverDuration,
-            mode: RunLoop.Mode.default
-        ) { event, stop in
-            guard let event = event else { return }
-            switch event.type {
-            case .leftMouseUp:
-                // Stop on a mouse up; post it back into the queue and call super so it can handle it
-                shouldCallSuper = true
-                NSApp.postEvent(event, atStart: false)
-                stop.pointee = true
-            case .leftMouseDragged:
-                // track mouse drags, and if more than a few points are moved we start a drag
-                let currentPoint = event.locationInWindow
-                guard let appHeight = NSApp.mainWindow?.frame.height else { return }
-                let threshold = appHeight - currentPoint.y
-                if threshold < 50 {
-                    if (abs(currentPoint.x - startingPoint.x) >= 5 || abs(currentPoint.y - startingPoint.y) >= 5) {
-                        stop.pointee = true
-                        window.performDrag(with: event)
-                    }
-                } else {
-                    shouldCallSuper = true
-                    stop.pointee = true
-                }
-            default:
-                break
-            }
+        guard let window = event.window else { return }
+        let top = window.frame.height - event.locationInWindow.y;
+        if (top < 50) {
+            window.performDrag(with: event)
         }
-        
-        if (shouldCallSuper) {
-            super.mouseDown(with: event)
-        }
+        super.mouseDown(with: event)
     }
 }
