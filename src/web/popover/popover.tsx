6dad45e88f0e1fa61d709c5c76cc9f8b1522654a
Thien Do <5953369+thien-do@users.noreply.github.com>
2022-09-03 15:37:12 +0700

    fix(web): Fix popover unmount animation

--- src/web/popover/popover.tsx
+++ src/web/popover/popover.tsx
@@ -1,34 +1,34 @@
 import * as Radix from "@radix-ui/react-popover";
+import { forwardRef } from "react";
 import { animation } from "../animation/animation";
 import { Card } from "../card/card";
-import * as s from "./popover.module.css";
-import "./popover.css";
-import { ReactNode } from "react";
 import { Scroll } from "../scroll/scroll";
+import "./popover.css";
+import * as s from "./popover.module.css";
 
-interface Props {
-  children: ReactNode;
-}
-
-const Content = (props: Props): JSX.Element => {
-  const { children } = props;
-  return (
-    <Radix.Content
-      className={[s.container, Card.glass, animation.flip].join(" ")}
-      sideOffset={12}
-      collisionPadding={12}
-    >
-      <Scroll.Root>
-        <Scroll.Viewport>
-          <div className={s.content}>{children}</div>
-        </Scroll.Viewport>
-        <Scroll.Track orientation="vertical">
-          <Scroll.Thumb />
-        </Scroll.Track>
-      </Scroll.Root>
-    </Radix.Content>
-  );
-};
+const Content = forwardRef<HTMLDivElement, Radix.PopoverContentProps>(
+  (props, ref): JSX.Element => {
+    const { children, ...rest } = props;
+    return (
+      <Radix.Content
+        sideOffset={12}
+        collisionPadding={12}
+        className={[s.container, Card.glass, animation.flip].join(" ")}
+        {...rest}
+        ref={ref}
+      >
+        <Scroll.Root>
+          <Scroll.Viewport>
+            <div className={s.content}>{children}</div>
+          </Scroll.Viewport>
+          <Scroll.Track orientation="vertical">
+            <Scroll.Thumb />
+          </Scroll.Track>
+        </Scroll.Root>
+      </Radix.Content>
+    );
+  }
+);
 
 export const Popover = {
   Content,
