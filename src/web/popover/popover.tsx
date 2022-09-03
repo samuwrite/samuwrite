8767f0a0fbdf4ef1a3995331d9b048251bad24ec
Thien Do <5953369+thien-do@users.noreply.github.com>
2022-09-03 15:24:52 +0700

    fix(web): Use custom scroll inside Popover

--- src/web/popover/popover.tsx
+++ src/web/popover/popover.tsx
@@ -1,15 +1,34 @@
 import * as Radix from "@radix-ui/react-popover";
 import { animation } from "../animation/animation";
 import { Card } from "../card/card";
-import { patch } from "../utils/patch";
 import * as s from "./popover.module.css";
 import "./popover.css";
+import { ReactNode } from "react";
+import { Scroll } from "../scroll/scroll";
 
-const Content = patch(Radix.Content, {
-  className: [s.content, Card.glass, animation.flip].join(" "),
-  sideOffset: 12,
-  collisionPadding: 12,
-});
+interface Props {
+  children: ReactNode;
+}
+
+const Content = (props: Props): JSX.Element => {
+  const { children } = props;
+  return (
+    <Radix.Content
+      className={[s.container, Card.glass, animation.flip].join(" ")}
+      sideOffset={12}
+      collisionPadding={12}
+    >
+      <Scroll.Root>
+        <Scroll.Viewport>
+          <div className={s.content}>{children}</div>
+        </Scroll.Viewport>
+        <Scroll.Track orientation="vertical">
+          <Scroll.Thumb />
+        </Scroll.Track>
+      </Scroll.Root>
+    </Radix.Content>
+  );
+};
 
 export const Popover = {
   Content,
